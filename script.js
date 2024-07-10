b23425772c3b87bf12745391063b5633ea5e7f51
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-07-10 17:08:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+function greet(){
+return `Hello, my name is ${name}, I am ${age} years old.`
+}
+	
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	function jobGreet(){
+return `Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}`
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
